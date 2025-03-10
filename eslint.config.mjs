27e4d2a07ac3c9af79728603ad7fcb9bb00a5268
RodrigoRafaelSantos7 <rodrigorafaelsantos7@icloud.com>
2025-03-10 23:10:19 +0000

    Add ESLint file naming and folder conventions
    - Integrate `eslint-plugin-check-file` for filename and folder naming rules
    - Configure kebab-case naming convention for TypeScript files and folders
    - Add new script for Prettier formatting
    - Update package dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import { FlatCompat } from "@eslint/eslintrc";
+import checkFile from "eslint-plugin-check-file";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -12,11 +13,29 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
   {
+    plugins: {
+      "check-file": checkFile,
+    },
     rules: {
       "prefer-arrow-callback": ["error"],
       "prefer-template": ["error"],
       semi: ["error"],
       quotes: ["error", "double"],
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE",
+        },
+        {
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          "src/**/!^[.*": "KEBAB_CASE",
+        },
+      ],
     },
   },
 ];
